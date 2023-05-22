ce998a583cc21f1472efcc14f6a9039463da49ea
Dany <dany21sayan_@hotmail.com>
2023-05-22 15:35:51 +0700

    first views change

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response 
 from . import serializers
 from . import models
 
