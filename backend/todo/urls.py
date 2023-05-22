518a79bf0e0375e276c4a90e036561d3941ef0e3
Dany <dany21sayan_@hotmail.com>
2023-05-22 16:43:17 +0700

    end of APIView convertion

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
-from . import views
+from .views import TodoList, TodoDetail
 from rest_framework import routers
-
-router= routers.DefaultRouter()
-router.register('todo', views.TodoViewSet, basename='todo')
 
 
 urlpatterns = [
+    path('todo/', TodoList.as_view(), name='todo-list'),
+    path('todo/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
 ]
 
-urlpatterns += router.urls
